21964477b8cc3b82f97c34c993f18f822a15369a
Kevin Shan <779973772@qq.com>
2023-04-13 11:07:43 +0800

    udpate vimrc

--- vimrc.sh
+++ vimrc.sh
@@ -1,1 +1,3 @@
-echo -e 'set number\nset hlsearch\nsyntax on\nset hlsearch\ncolorscheme desert\nset nocompatible\nset backspace=indent,eol,start\nset tabstop=4\nset autoindent' > ~/.vimrc
+#!/bin/bash
+
+echo 'set number\nset hlsearch\nsyntax on\nset hlsearch\ncolorscheme desert\nset nocompatible\nset backspace=indent,eol,start\nset tabstop=4\nset autoindent' > ~/.vimrc
